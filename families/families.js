2b7efb07c29598250011af235d2f88ccec274e4a
Cody-Wise <cody@sharptackmedia.com>
2022-03-30 12:53:41 -0700

    Finished Fetch Utils

--- families/families.js
+++ families/families.js
@@ -9,13 +9,45 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-function displayFamilies() {
+async function displayFamilies(families) {
     // fetch families from supabase
-
+    await getFamilies();
     // clear out the familiesEl
+    familiesEl.textContent = '';
+
 
     for (let family of families) {
         // create three elements for each family, one for the whole family, one to hold the name, and one to hold the bunnies
+        const familyEl = document.createElement('div');
+        const nameEl = document.createElement('h3');
+        const bunniesEl = document.createElement('div');
+
+        familyEl.classList.add('family');
+        bunniesEl.classList.add('bunnies');
+        nameEl.textContent = family.name;
+        for (let bunny of family.fuzzy_bunnies){
+            const bunnyEl = document.createElement('div');
+            bunnyEl.classList.add('bunny');
+            bunnyEl.textContent = bunny.name;
+
+            bunnyEl.addEventListener('click', async () => {
+               await deleteBunny(bunny.id);
+
+
+               const updatedFamilies = await getFamilies();
+               displayFamilies(updatedFamilies);
+                
+            });
+
+            bunniesEl.append(bunnyEl);
+
+
+            familyEl.append(nameEl, bunniesEl);
+
+            familiesEl.append(familyEl);
+         
+
+        }
         // your HTML Element should look like this:
         // <div class="family">
         //    <h3>the Garcia family</h3>
